afdb4a991eab189c26cde3670c656dd5ea282b0d
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-06-27 11:36:34 -0500

    Update to 1.21

--- src/main/java/com/teamabode/rodsnreels/RodsNReels.java
+++ src/main/java/com/teamabode/rodsnreels/RodsNReels.java
@@ -3,13 +3,8 @@ package com.teamabode.rodsnreels;
 import com.google.common.reflect.Reflection;
 import com.teamabode.rodsnreels.core.registry.RNRFeatures;
 import com.teamabode.rodsnreels.core.registry.RNRPlacedFeatures;
-import com.teamabode.rodsnreels.core.registry.tag.RNRBiomeTags;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModification;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +18,6 @@ public class RodsNReels implements ModInitializer {
     }
 
     public static ResourceLocation id(String name) {
-        return new ResourceLocation(MOD_ID, name);
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
     }
 }
