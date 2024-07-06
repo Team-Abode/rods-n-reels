66090f1e71a821c074482e7b56f0f11cec7d1387
Chikorita Lover <cjgardels@gmail.com>
2024-07-06 16:28:37 -0500

    Update raw squid and calamari
    Added calamari recipes for smelting, smoking, and campfire cooking
    Squids and glow squids now drop raw squid upon death (or calamari if on fire)
    Changed item IDs to be consistent with those of vanilla

--- src/main/java/com/teamabode/rodsnreels/common/event/RNRLootTableEvents.java
+++ src/main/java/com/teamabode/rodsnreels/common/event/RNRLootTableEvents.java
@@ -1,19 +1,16 @@
 package com.teamabode.rodsnreels.common.event;
 
+import com.teamabode.rodsnreels.core.registry.RNRLootTables;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
-import net.minecraft.world.level.storage.loot.providers.number.NumberProviders;
+import net.minecraft.world.level.storage.loot.entries.NestedLootTable;
 
 public class RNRLootTableEvents {
-
     public static void modifyVanillaLootTables() {
-        LootTableEvents.MODIFY.register((id, builder, source) -> {
-            if (EntityType.SQUID.getDefaultLootTable().equals(id)) {
-
+        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+            if (key.equals(EntityType.SQUID.getDefaultLootTable()) || key.equals(EntityType.GLOW_SQUID.getDefaultLootTable())) {
+                tableBuilder.withPool(LootPool.lootPool().add(NestedLootTable.lootTableReference(RNRLootTables.SQUID_MEAT)));
             }
         });
     }
