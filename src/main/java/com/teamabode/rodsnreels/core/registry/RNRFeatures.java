525c015897a53556f6eaa40eb4e1c35fd2abe994
Outer Cloud <outercloudstudio@gmail.com>
2024-09-03 00:31:55 -0500

    Working Ruin Feature

--- src/main/java/com/teamabode/rodsnreels/core/registry/RNRFeatures.java
+++ src/main/java/com/teamabode/rodsnreels/core/registry/RNRFeatures.java
@@ -1,15 +1,18 @@
 package com.teamabode.rodsnreels.core.registry;
 
 import com.teamabode.rodsnreels.RodsNReels;
+import com.teamabode.rodsnreels.common.feature.RuinFeature;
 import com.teamabode.rodsnreels.common.feature.SuspiciousBlockConfiguration;
 import com.teamabode.rodsnreels.common.feature.SuspiciousBlockFeature;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
 public class RNRFeatures {
     public static final Feature<SuspiciousBlockConfiguration> SUSPICIOUS_BLOCK = register("suspicious_block", new SuspiciousBlockFeature());
+    public static final Feature<NoneFeatureConfiguration> RUIN = register("ruin", new RuinFeature());
 
     public static void register() {}
 
