cb854f6504df63205c8e9eb695a3753f8b440371
Outer Cloud <outercloudstudio@gmail.com>
2024-09-04 01:08:07 -0500

    Working Custom Structure

--- src/main/java/com/teamabode/rodsnreels/common/structure/Ruin.java
+++ src/main/java/com/teamabode/rodsnreels/common/structure/Ruin.java
@@ -3,9 +3,16 @@ package com.teamabode.rodsnreels.common.structure;
 import com.mojang.serialization.MapCodec;
 import com.teamabode.rodsnreels.RodsNReels;
 import com.teamabode.rodsnreels.core.registry.RNRStructures;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
+import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.templatesystem.LiquidSettings;
 
 import java.util.Optional;
 
@@ -27,6 +34,13 @@ public class Ruin extends Structure {
     }
 
     private static void generatePieces(StructurePiecesBuilder structurePiecesBuilder, Structure.GenerationContext generationContext) {
-        RodsNReels.LOGGER.info(String.valueOf(generationContext.chunkPos()));
+        structurePiecesBuilder.addPiece(new PoolElementStructurePiece(
+                generationContext.structureTemplateManager(),
+                SinglePoolElement.single(RodsNReels.id("ruin_pillar").toString()).apply(StructureTemplatePool.Projection.RIGID),
+                generationContext.chunkPos().getBlockAt(0 , 63, 0), 2,
+                Rotation.NONE,
+                BoundingBox.infinite(),
+                LiquidSettings.APPLY_WATERLOGGING
+        ));
     }
 }
